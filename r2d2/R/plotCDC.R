e2d67b3b4e960f3d85feb6645e47242c659588bd
Rohit Doobay <rdoobay@mail.usf.edu>
2018-03-25 21:42:33 -0400

    Using Depends

--- r2d2/R/plotCDC.R
+++ r2d2/R/plotCDC.R
@@ -7,8 +7,8 @@
 #' @import ggplot2
 plot.CDC.data <- function(ds){
   ch.title <- paste("Main Causes of Deaths in", ds[1,4], "for the year",ds[1,1])
-  g <- ggplot2::ggplot(ds,ggplot2::aes(Cause, Deaths,fill= Cause ))
-  g +  ggplot2::geom_bar(stat="identity", position = 'dodge') +
-    ggplot2::theme(axis.text.x=ggplot2::element_text(angle=90,hjust=1)) +
-    ggplot2::ggtitle(ch.title)
+  g <- ggplot(ds,aes(Cause, Deaths,fill= Cause ))
+  g +  geom_bar(stat="identity", position = 'dodge') +
+    theme(axis.text.x=element_text(angle=90,hjust=1)) +
+    ggtitle(ch.title)
 }
